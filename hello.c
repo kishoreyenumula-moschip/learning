6223f7b1d7517c65c179e9e316b88267825b7045
kishoreyenumula <kishore.yenumula@gmail.com>
2023-11-01 11:12:33 +0530

    added new file to remotely created branch and pushing it back

---
+++ hello.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void main()
+{
+	printf("this branch is created remotely \n");
+
+	/* this branch is created remotely and fetched into the our 
+	 * terminal using command 
+	 * $git fetch origin branch_name
+	 */ 
+}
